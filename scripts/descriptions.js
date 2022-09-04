1712b6033154f57b5cdc1672028ea50ae77eec0a
mogs.git <mogs.mail1@gmail.com>
2022-09-04 22:14:00 +0100

    mobile css formatting

--- scripts/descriptions.js
+++ scripts/descriptions.js
@@ -1,4 +1,4 @@
 let arr = []
-arr.push({post_link: '../writing/travelling-in-norway-part-2.html',post_name: 'Travelling in Norway - Part 2',post_text:'Travelling from Jotunheim to Rondane.',post_date:new Date("2022/09/01")})
-arr.push({post_link: '../writing/travelling-in-norway-part-1.html',post_name: 'Travelling in Norway - Part 1',post_text:'Leaving my job to travel solo in Norway.',post_date:new Date("2022/09/01")})
-arr.push({post_link: '../writing/introduction-to-the-blog.html',post_name: 'Introduction to the blog',post_text:'A brief intro to the blog.',post_date:new Date("2022/08/30")})
+arr.push({post_link: 'writing/travelling-in-norway-part-2.html',post_name: 'Travelling in Norway - Part 2',post_text:'Travelling from Jotunheim to Rondane.',post_date:new Date("2022/09/01")})
+arr.push({post_link: 'writing/travelling-in-norway-part-1.html',post_name: 'Travelling in Norway - Part 1',post_text:'Leaving my job to travel solo in Norway.',post_date:new Date("2022/09/01")})
+arr.push({post_link: 'writing/introduction-to-the-blog.html',post_name: 'Introduction to the blog',post_text:'A brief intro to the blog.',post_date:new Date("2022/08/30")})
--- scripts/get_descriptions.py
+++ scripts/get_descriptions.py
@@ -16,7 +16,7 @@ for bp in dir_list:
     desc_text = lines[lines.find('<meta name="description" content="')+len('<meta name="description" content="') : lines.find('"></meta><!--desc-->')].replace("\n", "")
     blog_title = lines[lines.find('<title>')+len('<title>') : lines.find('</title>')].replace("\n", "")
     blog_date = lines[lines.find('<meta name="date" content=')+len('<meta name="date" content=') : lines.find('><!--date-->')].replace("\n", "")
-    dict_string += "arr.push({post_link: '../writing/" + bp + "',post_name: '"+ blog_title + "',post_text:'" + desc_text + "',post_date:new Date(" + blog_date + ")})\n"
+    dict_string += "arr.push({post_link: 'writing/" + bp + "',post_name: '"+ blog_title + "',post_text:'" + desc_text + "',post_date:new Date(" + blog_date + ")})\n"
     arr.append("arr.push("+ bp + "," + desc_text + ",," + blog_date + ")")
 
 print(dict_string)
